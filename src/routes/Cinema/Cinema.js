045e86d4b9c948facfa28c3f315562fae5f82f3b
jiliangmason <jiliangmason@163.com>
2017-08-01 23:33:51 +0800

    cinema detail

--- src/routes/Cinema/Cinema.js
+++ src/routes/Cinema/Cinema.js
@@ -11,7 +11,10 @@ import {connect} from 'dva';
 
 class Cinema extends React.Component{
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      area: ''
+    }
   }
 
   componentDidMount() {
@@ -20,11 +23,56 @@ class Cinema extends React.Component{
     dispatch({type: 'cinema/fetchCinemaDetail', payload: {id: id}})
   };
 
+  shouldComponentUpdate(nextProps, nextState) {
+    return ((nextProps.district !== this.props.district) || (nextProps.list !== this.props.list) || (nextState.area !== this.state.area))
+  }
+
+  changeDistrict(pinyin) {
+    this.setState({
+      area: this.state.area == pinyin ? '' : pinyin
+    })
+  }
+
   render() {
-    const {} = this.props;
+    const {district, list} = this.props;
+    const {id} = this.props.location.query;
+
+    let disList = district.length && district.map((items, index)=>{
+          return (<div key={index} className={styles.item}>
+            <div className={styles.title} onClick={this.changeDistrict.bind(this, items.pinyin)}>
+              {items.name}
+            </div>
+            <div className={this.state.area == items.pinyin ? `${styles.list} ${styles.active}` : styles.list}>
+              {
+                list.length && list.map((item, index)=>{
+                  if (items.pinyin == item.district.pinyin) {
+                    return (<div key={index} className={styles.shop}>
+                      <a href={`http://m.maizuo.com/v4/?co=maizuo#!/film/${id}/cinema`} >
+                        <div className={styles.desc}>
+                          <div className={styles.title}>{item.name} <span>座</span>{(item.itemTypes.length>3)&&<span>通</span>}</div>
+                          <div className={styles.tip}>
+                            {
+                              item.labels.length && item.labels.map((ele, index)=>{
+                                return (<span key={index} className={ele.type.toLocaleLowerCase() == 'sundry' ? styles.sundry : styles.discount}>{ele.name}</span>)
+                              })
+                            }
+                          </div>
+                          <div className={styles.area}>{item.address}</div>
+                          <div className={styles.extra}>距离未知 | 剩余{item.avaliableSchedule}场</div>
+                        </div>
+                        <div className={styles.price}>￥{item.minimumPrice}</div>
+                      </a>
+                    </div>)
+                  }
+
+                })
+              }
+            </div>
+          </div>)
+      });
     return (
       <div id={styles.cinema}>
-        cinema
+        {disList}
       </div>
     )
   }
